1c15f72cced2526352d1019a3476b94ea8524462
mandy <mandyyu9502@gmail.com>
2025-12-26 16:06:42 +0800

    runenv

--- google_oauth.py
+++ google_oauth.py
@@ -1,10 +1,12 @@
-#google_oauth.py
 import os
+import requests
 from google.oauth2 import id_token
 from google.auth.transport import requests as google_requests
 from fastapi import HTTPException, status
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET=os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_TOKEN_URL="https://oauth2.googleapis.com/token"
 
 def verify_google_id_token(token: str):
     """驗證從前端或 Postman 傳來的 Google ID Token"""
@@ -20,3 +22,20 @@ def verify_google_id_token(token: str):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="無效的 Google Token"
         )
+    
+def exchange_code_for_tokens(code: str,redirect_uri: str)->dict:
+    payload={
+        "code":code,
+        "client_id":GOOGLE_CLIENT_ID,
+        "client_secret":GOOGLE_CLIENT_SECRET,
+        "redirect_uri":redirect_uri,
+        "grant_type":"authorization_code"
+    }
+    response=requests.post(GOOGLE_TOKEN_URL,data=payload)
+    if response.status_code!=200:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"無法換取 Token:{response.json().get('erroe_description','未知錯誤')}"
+        
+        )
+    return response.json()
